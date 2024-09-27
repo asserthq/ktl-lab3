3611dc897ad35ab249331ffabd4d27fa3464fc5f
zdrvzbl <sanya2003ogorodov@gmail.com>
2024-09-27 22:17:16 +0700

    isPalindrom func fix

--- app/src/main/java/com/example/lab3/util/util.kt
+++ app/src/main/java/com/example/lab3/util/util.kt
@@ -1,10 +1,13 @@
 package com.example.lab3.util
 
 fun isPalindrom(str: String): Boolean {
-    val str = str.filterNot { sym -> sym == ' ' }.lowercase()
+    val prepStr = str.filterNot { sym -> sym == ' ' }.lowercase()
     var ret = true
-    for (i in 0..str.length / 2) {
-        ret = str[i] == str.reversed()[i]
+    for (i in 0..<prepStr.length / 2) {
+        if (prepStr[i] != prepStr.reversed()[i]) {
+            ret = false
+            break
+        }
     }
     return ret
 }
