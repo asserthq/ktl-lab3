9ac9536496fb8a0cf0eba4f139fd6acb218d6e03
zdrvzbl <sanya2003ogorodov@gmail.com>
2024-09-27 22:20:47 +0700

    fix task logic in func isPalindrom

--- app/src/main/java/com/example/lab3/util/util.kt
+++ app/src/main/java/com/example/lab3/util/util.kt
@@ -1,7 +1,7 @@
 package com.example.lab3.util
 
 fun isPalindrom(str: String): Boolean {
-    val prepStr = str.filterNot { sym -> sym == ' ' }.lowercase()
+    val prepStr = str.filter { sym -> sym.isLetter() }.lowercase()
     var ret = true
     for (i in 0..<prepStr.length / 2) {
         if (prepStr[i] != prepStr.reversed()[i]) {
